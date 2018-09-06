39051d45f20ea79d07a7069ff0243a21d3a980c8
Feno <csfeno@gmail.com>
2018-09-06 15:54:40 +0200

    Interstitial Ad Script

---
+++ Recoil/Assets/03_Scripts/InterstitialAd.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class InterstitialAd : MonoBehaviour
+{
+    public GameObject container;
+    public Text text;
+    public int gamePlayed;
+
+    public GameManager gameManager;
+    public string[] texts;
+    private int index;
+    // Use this for initialization
+
+    void Start()
+    {
+        gamePlayed = PlayerPrefs.GetInt("gamePlayed");
+        container = transform.GetChild(0).gameObject;
+        text = container.GetComponentInChildren<Text>();
+        gameManager.OnLevelEnd += OnLevelEnd;
+    }
+
+    void OnLevelEnd()
+    {
+        Debug.Log(gamePlayed);
+        gamePlayed++;
+        PlayerPrefs.SetInt("gamePlayed", gamePlayed);
+        index = texts.Length;
+
+        if (gamePlayed % Mathf.Max(Mathf.Round(100 / gamePlayed), 3) == 0 && Advertisement.IsReady())
+            StartCoroutine(ShowInterstitialAd());
+    }
+
+    IEnumerator ShowInterstitialAd()
+    {
+        container.SetActive(true);
+        for (int i = index - 1; i >= 0; i--)
+        {
+            text.text = texts[i];
+            yield return new WaitForSeconds(1f);
+        }
+
+        ShowOptions adOption = new ShowOptions();
+        adOption.resultCallback = (ShowResult res) =>
+        {
+            container.SetActive(false);
+        };
+        Advertisement.Show("interstitials", adOption);
+    }
+}
